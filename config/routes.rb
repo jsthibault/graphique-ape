efbb81b7048a7077d1513396d3f30faffae69a60
Mowgli <jean.stephane.thib@gmail.com>
2016-11-29 12:02:45 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'graphiques/tech3'
+  get 'graphiques/tech2' => 'graphiques#tech3'
   root 'graphiques#tech3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
